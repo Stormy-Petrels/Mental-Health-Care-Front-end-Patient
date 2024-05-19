fec42981fb35a445bd375467789d00b1c74bc6e7
luantranthannh <luan.tran25@student.passerellesnumeriques.org>
2024-05-19 17:54:54 +0700

    add Footer component and Sign in Sign up

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import Navbar from './layouts/Navbar';
 import MainLayout from './layouts/MainLayout';
+import Footer from './layouts/Footer';
 function App() {
   return (
     <div className="App">
       <Navbar />
       <MainLayout />
+      <Footer />
     </div>
   );
 }
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -7,13 +7,16 @@ import NotFound from "../pages/NotFound";
 import Home from "../pages/Home";
 // import Search from "../users/Search";
 // import User from "../users/User";
+import DoctorDetail from "./../pages/DoctorDetail";
 const MainLayout = () => {
   return (
     <div className="container">
       <Switch>
         {/* <Route exact path="/" component={Search} /> */}
         <Route exact path="/" component={Home} />
-        <Route exact path="/doctors" component={Doctors} />
+        <Route exact path="/doctors" component={Doctors}>
+          <Route exact path="/doctor:doctorId" component={DoctorDetail} />
+        </Route>
         <Route exact path="/about" component={About} />
         <Route exact path="/contact" component={Contact} />
         <Route path="/*" component={NotFound}></Route>
