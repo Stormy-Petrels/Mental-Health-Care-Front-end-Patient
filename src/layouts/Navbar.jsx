4d886712ecffc0c282be6dff3577c08db0a4b35b
Hoai <hoai.nguyen25@student.passerellesnumeriques.org>
2024-05-24 08:41:31 +0700

    THe homepage of patient

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -6,16 +6,13 @@ function Navbar() {
   return (
     <div>
       <header className="bg-white">
-        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-screen-2xl px-4 sm:px-6 lg:px-8">
           <div className="flex h-16 items-center justify-between">
             <div className="md:flex md:items-center md:gap-12">
-              <a
-                className="flex items-center justify-center text-teal-600"
-                href="#"
-              >
+              <a className="flex items-center justify-center text-teal-600" href="#">
                 <span className="sr-only">Home</span>
                 <Logo />
-                <p className="text-[#1CBBD0] ml-2 text-2xl">
+                <p className="text-cyan-500 ml-2 text-2xl">
                   Mental Health Care
                 </p>
               </a>
@@ -25,35 +22,23 @@ function Navbar() {
               <nav aria-label="Global">
                 <ul className="flex items-center gap-6 text-sm">
                   <Link to="/">
-                    <a
-                      className="text-size-small text-stone-500 transition hover:text-gray-500/75"
-                      href="#"
-                    >
+                    <a className="text-size-small text-stone-500 transition hover:text-gray-500/75" href="#">
                       Home
                     </a>
                   </Link>
 
                   <Link to="/doctors">
-                    <a
-                      className="text-size-small text-stone-500 transition hover:text-gray-500/75"
-                      href="#"
-                    >
+                    <a className="text-size-small text-stone-500 transition hover:text-gray-500/75" href="#">
                       Doctors
                     </a>
                   </Link>
                   <Link to="/about">
-                    <a
-                      className="text-size-small text-stone-500 transition hover:text-gray-500/75"
-                      href="#"
-                    >
+                    <a className="text-size-small text-stone-500 transition hover:text-gray-500/75" href="#">
                       About
                     </a>
                   </Link>
                   <Link to="/contact">
-                    <a
-                      className="text-size-small text-stone-500 transition hover:text-gray-500/75"
-                      href="#"
-                    >
+                    <a className="text-size-small text-stone-500 transition hover:text-gray-500/75" href="#">
                       Contact
                     </a>
                   </Link>
