b06a5122a94017bdc754b05b97297e51363cfdce
Hoai <hoai.nguyen25@student.passerellesnumeriques.org>
2024-05-24 13:28:13 +0700

    Added scrolling feature in the Navbar

--- src/pages/SlidePanel.jsx
+++ src/pages/SlidePanel.jsx
@@ -1,8 +1,26 @@
 import { Button } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const SlidePanel = () => {
+    const [timeSlots, setTimeSlots] = useState([]);
+
+    useEffect(() => {
+      const fetchTimeSlots = async () => {
+        try {
+          const response = await axios.post('/api/checkTime', {
+            date: '2024-05-24',
+            doctorId: 1
+          });
+          setTimeSlots(response.data.listTime);
+        } catch (error) {
+          console.error('Error fetching time slots:', error);
+        }
+      };
+  
+      fetchTimeSlots();
+    }, []);
   return (
     <div className="bg-white shadow-md shadow-gray-500 p-3 lg:p-5 rounded-md">
         <div className="flex items-center justify-between">
@@ -18,22 +36,16 @@ const SlidePanel = () => {
             </p>
 
             <ul className="py-3">
-                <li className="flex items-center justify-between mb-2">
+                {timeSlots.map((slot) => (
+                    <li key={slot.id} className="flex items-center justify-between mb-2">
                     <p className="text-base leading-6 text-textColor">
-                        Sunday
+                        {new Date(slot.timeStart).toLocaleDateString('en-US', { weekday: 'long' })}
                     </p>
                     <p className="text-base leading-6 text-textColor">
-                        4:00 PM - 9:30 PM
+                        {new Date(slot.timeStart).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })} - {new Date(slot.timeEnd).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
                     </p>
-                </li>
-                <li className="flex items-center justify-between mb-2">
-                    <p className="text-base leading-6 text-textColor">
-                        Sunday
-                    </p>
-                    <p className="text-base leading-6 text-textColor">
-                        4:00 PM - 9:30 PM
-                    </p>
-                </li>
+                    </li>
+                ))}
             </ul>
         </div>
         <Button variant="contained" className=" w-full rounded-md">
